6b687daea7bae0dfd9aa27f526629c754d19ba5a
SzymonMielecki <58358829+SzymonMielecki@users.noreply.github.com>
2023-12-02 17:20:05 +0100

    prep for all remaining days

---
+++ day_25/src/lib.rs
@@ -0,0 +1,24 @@
+pub fn process_part1(input: &str) -> String {
+    input.into()
+}
+pub fn process_part2(input: &str) -> String {
+    input.into()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part1() {
+        let input = "";
+        let processed = process_part1(&input);
+        assert_eq!(processed, "")
+    }
+    #[test]
+    fn part2() {
+        let input = "";
+        let processed = process_part2(&input);
+        assert_eq!(processed, "")
+    }
+}
