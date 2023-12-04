348a175b452398a0aa8f39d4f4b515850e122241
SzymonMielecki <58358829+SzymonMielecki@users.noreply.github.com>
2023-12-04 09:43:17 +0100

    wasdfkjasdfjk

--- day_04/src/lib.rs
+++ day_04/src/lib.rs
@@ -8,12 +8,13 @@ use nom::{
     IResult,
 };
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Game {
     id: u32,
     winnging_nums: Vec<u32>,
     my_nums: Vec<u32>,
     mutual: Vec<u32>,
+    copies: u32,
 }
 impl Game {
     pub fn deconstructed(res: IResult<&str, (u32, (Vec<u32>, Vec<u32>))>) -> Self {
@@ -29,18 +30,9 @@ impl Game {
             winnging_nums,
             my_nums,
             mutual,
+            copies: 1,
         }
     }
-}
-
-pub fn process_part1(input: &str) -> String {
-    input
-        .lines()
-        .map(|l| Game::deconstructed(parse_line(l)))
-        .filter(|x| x.mutual.len() > 0)
-        .map(|x| (2 as usize).pow(x.mutual.len() as u32 - 1))
-        .sum::<usize>()
-        .to_string()
 }
 
 fn parse_line(input: &str) -> IResult<&str, (u32, (Vec<u32>, Vec<u32>))> {
@@ -53,8 +45,39 @@ fn parse_line(input: &str) -> IResult<&str, (u32, (Vec<u32>, Vec<u32>))> {
         ),
     ))(input)
 }
+
+pub fn process_part1(input: &str) -> String {
+    input
+        .lines()
+        .map(|l| Game::deconstructed(parse_line(l)))
+        .filter(|x| x.mutual.len() > 0)
+        .map(|x| (2 as usize).pow(x.mutual.len() as u32 - 1))
+        .sum::<usize>()
+        .to_string()
+}
+
 pub fn process_part2(input: &str) -> String {
-    input.into()
+    let mut winning: Vec<Game> = input
+        .lines()
+        .map(|l| Game::deconstructed(parse_line(l)))
+        .collect();
+
+    for i in 0..winning.len() {
+        for j in 1..=(winning[i].copies + winning[i].mutual.len() as u32) {
+            if let Some(n) = winning.iter_mut().find(|x| x.id == i as u32 + j + 1) {
+                println!("added {i}, {j}");
+                n.copies += 1
+            }
+        }
+    }
+
+    dbg!(&winning);
+    winning
+        .iter()
+        .filter(|x| x.mutual.len() > 0)
+        .map(|w| w.copies)
+        .sum::<u32>()
+        .to_string()
 }
 
 #[cfg(test)]
@@ -74,8 +97,13 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
     }
     #[test]
     fn part2() {
-        let input = "";
+        let input = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
         let processed = process_part2(&input);
-        assert_eq!(processed, "")
+        assert_eq!(processed, "30")
     }
 }
